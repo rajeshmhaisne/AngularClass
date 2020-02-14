b228894967cfe43b70ff7db4b323f5147f3bb4af
rajeshmhaisne <rajesh_mhaisne@hotmail.com>
2020-02-14 10:35:02 +0530

    14-Feb-2020 class work final

--- src/app/directives/host.directive.ts
+++ src/app/directives/host.directive.ts
@@ -1,10 +1,24 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Renderer, HostListener } from "@angular/core";
 
 @Directive({
-  selector: '[appHost]'
+  selector: "[appHost]"
 })
 export class HostDirective {
+  constructor(private el: ElementRef, private renderer: Renderer) {}
 
-  constructor() { }
+  @HostListener("mouseover") onMouseOver() {
+    this.ChangeBgColor("red");
+  }
 
+  @HostListener("click") onClick() {
+    window.alert("element clicked");
+  }
+
+  @HostListener("mouseleave") onMouseLeave() {
+    this.ChangeBgColor("black");
+  }
+
+  ChangeBgColor(color: string) {
+    this.renderer.setElementStyle(this.el.nativeElement, "color", color);
+  }
 }
--- src/app/products/mob-parts/mob-parts.component.ts
+++ src/app/products/mob-parts/mob-parts.component.ts
@@ -16,7 +16,30 @@ import { MobParts } from "./mob-parts";
   ]
 })
 export class MobPartsComponent {
-  mobParts: MobParts[] = MOBPARTS;
+  mobParts: MobParts[];
+
+  constructor() {
+    console.log("Constructor called");
+  }
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    console.log("ngOnInit called");
+    this.mobParts = MOBPARTS;
+  }
+
+  ngOnChanges(): void {
+    //Called before any other lifecycle hook. Use it to inject dependencies, but avoid any serious work here.
+    //Add '${implements OnChanges}' to the class.
+    console.log("ngOnChanges called");
+  }
+
+  ngOnDestroy(): void {
+    //Called once, before the instance is destroyed.
+    //Add 'implements OnDestroy' to the class.
+    this.mobParts = [];
+  }
 
   calcProd() {
     let sum = 0;
